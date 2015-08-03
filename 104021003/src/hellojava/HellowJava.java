779e7ae87c606e45dc1f2f3169225bcc51a53678
Dperer <104021003@live.asia.edu.tw>
2015-08-03 13:57:10 +0800

    第一程式

---
+++ 104021003/src/hellojava/HellowJava.java
@@ -0,0 +1,56 @@
+package hellojava;
+
+public class HellowJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¡@¡@¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@¡@");
+		System.out.println("¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@¡¸¡@¡@¡@");
+		System.out.println("¡@¡@¡@¡@¡¸¡@¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@");
+		System.out.println("¡¸¡¸¡¸¡¸¡@¡@¡@¡@¡@¡@¡@¡@¡@¡¸¡¸¡¸¡¸¡@¡@¡@");
+		System.out.println("¡@¡¸¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@");
+		System.out.println("¡@¡@¡¸¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@");
+		System.out.println("¡@¡@¡@¡¸¡@¡@¡@¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@");
+		System.out.println("¡@¡@¡¸¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@");
+		System.out.println("¡@¡¸¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@");
+		System.out.println("¡¸¡¸¡¸¡¸¡@¡@¡@¡@¡@¡@¡@¡@¡@¡¸¡¸¡¸¡¸¡@¡@¡@");
+		System.out.println("¡@¡@¡@¡@¡¸¡@¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@");
+		System.out.println("¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@¡¸¡@¡@¡@");
+		System.out.println("¡@¡@¡@¡@¡@¡@¡@¡@¡¸¡@¡@¡@¡@");
+		
+		System.out.println("¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡´¡´¡´¡´¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡´¡´¡´¡´¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡´¡´¡´¡³¡´¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡´¡´¡´¡´¡³¡³¡´¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡´¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡³¡³¡´¡³¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡³¡´¡³¡´¡³¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡´¡³¡³¡³¡´¡³¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡´¡´¡³¡³¡³¡³¡³¡´¡³¡³¡³¡³");
+		System.out.println("¡³¡³¡³¡³¡³¡³¡³¡³¡³¡³¡´¡³¡³¡³¡³");
+		
+		System.out.println(" ¡@¡@   ¢~¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢w¢¡");
+		System.out.println("    ¢x                     ¢x");
+		System.out.println("¡@¡@¡@¢¥¢¤¢¤¢¤¢¤¢¤¢¤¢¤¢¤¡ó¢¤¢¤¡ó¢¤¢¤¢¤¢¤¢¤¢¤¢¤¢§");
+		System.out.println("¡@¡@¡@¢x                     ¢x");
+		System.out.println(" ¡@¡@¡@¢x                ¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@¡@    ¢x");
+		System.out.println("¡@¡@¡@ ¢x          ¡Û¡@¡@¡@¡@¡@¡@¡@    ¢x");
+		System.out.println("¡@¢~¢w¢t          ¡@¡@¡@¡@   ¢¢¢w¢£            ¡@¡@¡@¡@¢u¢w¢¡");
+		System.out.println("¡@¢x¢~¢t                   ¡@¡@¡@¡@¡@¡@¡@¡@¡@             ¢u¢¡¢x");
+		System.out.println("¡@¢x¢x¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢x¢x");
+		System.out.println("¡@¢x¢x¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢x¢x");
+		System.out.println("¡@ ¡` ¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i ¡`");
+		System.out.println("¡@¡@¡@¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i¢i");
+		System.out.println("¡@¡@¡@¡@¡@¡@¡@¢x¢x           ¢x¢x"); 
+		System.out.println("¡@¡@¡@¡@¡@¡@¡@¢x¢x           ¢x¢x");
+		System.out.println("¡@¡@¡@¡@¡@¡@¢~ùöùö¢¡           ¡@¡@¡@    ¢~ùöùö¢¡");
+		System.out.println("¡@¡@¡@¡@¡@¡@¢¢¢w¢w¢£         ¡@¡@¡@      ¢¢¢w¢w¢£");
+		
+	}
+
+}
